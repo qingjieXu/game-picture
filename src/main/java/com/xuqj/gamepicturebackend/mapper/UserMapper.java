d2224465fd604a9d32f476c6cfe410295c0047d5
11394 <qingjiexu1@gmail.com>
2025-08-18 22:08:15 +0800

    用户名模块内容

---
+++ src/main/java/com/xuqj/gamepicturebackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.xuqj.gamepicturebackend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuqj.gamepicturebackend.model.entity.User;
+
+/**
+* @author 11394
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-08-18 16:14:15
+* @Entity generator.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
