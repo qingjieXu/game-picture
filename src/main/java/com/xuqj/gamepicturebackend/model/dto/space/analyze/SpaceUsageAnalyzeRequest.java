8660a18220a1b2f6b02450de23f9d634ae77b6c6
11394 <qingjiexu1@gmail.com>
2025-08-27 16:33:22 +0800

    空间分析

---
+++ src/main/java/com/xuqj/gamepicturebackend/model/dto/space/analyze/SpaceUsageAnalyzeRequest.java
@@ -0,0 +1,11 @@
+package com.xuqj.gamepicturebackend.model.dto.space.analyze;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class SpaceUsageAnalyzeRequest extends SpaceAnalyzeRequest {
+
+}
+
